c484440d0d40db1908f60eb97bfad007608aa9e9
luzihang123 <526154064@qq.com>
2020-02-27 18:36:02 +0800

    feature：跑通sklearn svm

--- 4_quant_stock/1_stock_svm/parse_db.py
+++ 4_quant_stock/1_stock_svm/parse_db.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     ts.set_token(TUSHARE_TOKEN)
     pro = ts.pro_api()
     # 设定获取日线行情的初始日期和终止日期，其中终止日期设定为昨天。
-    start_dt = '20190101'
+    start_dt = '20100101'
     time_temp = datetime.datetime.now() - datetime.timedelta(days=1)
     end_dt = time_temp.strftime('%Y%m%d')
     # 建立数据库连接,剔除已入库的部分
